da06d81f6f7157ba8fbd472d79e9d6102206c29a
theburningmonk <theburningmonk@gmail.com>
2013-11-07 00:43:39 +0000

    - cleaner solution for problem 9

--- Solutions/Problem9.hs
+++ Solutions/Problem9.hs
@@ -1,3 +1,2 @@
-f [x, y, z] = x < y && y < z && x^2 + y^2 == z^2
-candidates = filter f $ concatMap (\x -> map (\y -> [x, y, 1000-x-y]) [x..666]) [1..333]
-answer = foldl1 (*).head $ candidates
+f x y z = x < y && y < z && x^2 + y^2 == z^2
+answer = head [ x * y * z | x <- [1..333], y <- [x..499], z <- [1000 - x - y], f x y z ]
